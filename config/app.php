fe1362ceed4e643afa950f174638cdde0140ec06
Evolyuta <dima.97_97@mail.ru>
2019-06-27 11:52:29 +0300

    add download method

---
+++ config/app.php
@@ -0,0 +1,3 @@
+<?php
+
+const FILE_NAME = 'books';
--- library/download.php
+++ library/download.php
@@ -1,6 +1,13 @@
 <?php
+require "/config/app.php";
 require "/inc/lib.inc.php";
 require "/inc/config.inc.php";
+header('Content-Type: Application/vnd.ms-excel');
+header('Content-Disposition: attachment; filename="' . FILE_NAME . '.xlsx"');
+
+ob_clean();
+//flush();
+
 
 $books = selectItemsFromBooksForExcelFile();
 if ($books === false) {
@@ -11,9 +18,7 @@ if (!count($books)) {
     echo "Empty!";
     exit;
 }
-
-$data =[];
-
+$data = [];
 foreach ($books as $book) {
     $authorId = $book['author'];
     $author = getItemFromAuthors($authorId);
@@ -22,7 +27,38 @@ foreach ($books as $book) {
         . ' ' . $author['surname']
         . ' ' . $author['patronymic'];
     $book['author'] = $authorFullName;
-    $data[]=$book;
+    $data[] = $book;
 }
 
-var_dump($data);
+
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$phpExcel = new Spreadsheet();
+$phpExcel->getProperties()->setCreator('Лукин Дмитрий')
+    ->setTitle('Список книг')
+    ->setSubject('Книги')
+    ->setDescription('Вывод списка книг');
+$phpExcel->getActiveSheet()->setTitle("Лист 1");
+$phpExcel->setActiveSheetIndex(0)
+    ->setCellValue('A1', 'Название')
+    ->setCellValue('B1', 'ФИО автора')
+    ->setCellValue('C1', 'Количество страниц')
+    ->setCellValue('D1', 'Год выпуска')
+    ->setCellValue('E1', 'Издатель');
+$col = 2;
+foreach ($data as $dataRow) {
+    $phpExcel->setActiveSheetIndex(0)
+        ->setCellValueByColumnAndRow(1, $col, $dataRow['title'])
+        ->setCellValueByColumnAndRow(2, $col, $dataRow['author'])
+        ->setCellValueByColumnAndRow(3, $col, $dataRow['pages'])
+        ->setCellValueByColumnAndRow(4, $col, $dataRow['pubYear'])
+        ->setCellValueByColumnAndRow(5, $col, $dataRow['publisher']);
+    $col++;
+}
+$writer = new Xlsx($phpExcel);
+$writer->save('php://output');
+exit();
+
